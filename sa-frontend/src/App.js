ed77286bafd55c0e13791a4dc6cc3a51f433e927
rachit89 <115970922+rachit89@users.noreply.github.com>
2022-12-08 10:42:20 +0530

    Update App.js

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://52.90.180.217:31220/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
